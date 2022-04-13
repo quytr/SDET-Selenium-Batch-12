3b8f3e01453636f20c13f443962de020edfc533f
quytr <quy.truong310@gmail.com>
2022-04-12 22:05:31 -0400

    "Selenium  Class 09"

---
+++ src/com/syntax/class09/HandlingPaginations.java
@@ -0,0 +1,81 @@
+package com.syntax.class09;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class HandlingPaginations {
+    public static String url = "http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login";
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        // log in option
+        WebElement username = driver.findElement(By.id("txtUsername"));
+        username.sendKeys("Admin");
+        WebElement password = driver.findElement(By.xpath("//input[contains(@name, 'txtP')]"));
+        password.sendKeys("Hum@nhrm123");
+        WebElement login = driver.findElement(By.xpath("//input[@value='LOGIN']"));
+        login.click();
+
+        // navigating to add employee page
+        WebElement pimButton = driver.findElement(By.id("menu_pim_viewPimModule"));
+        pimButton.click();
+        WebElement addEmployeeButton = driver.findElement(By.id("menu_pim_addEmployee"));
+        addEmployeeButton.click();
+
+        // adding an employee and saving
+        WebElement firstName = driver.findElement(By.id("firstName"));
+        firstName.sendKeys("Diego");
+        WebElement lastName = driver.findElement(By.id("lastName"));
+        lastName.sendKeys("Asghar");
+
+        WebElement empIdField = driver.findElement(By.id("employeeId"));
+        String empId = empIdField.getAttribute("value");
+
+        WebElement saveButton = driver.findElement(By.id("btnSave"));
+        saveButton.click();
+
+        WebElement employeeListButton = driver.findElement(By.id("menu_pim_viewEmployeeList"));
+        employeeListButton.click();
+
+
+        boolean flag = true;
+        while (flag) {
+            //Every time we move to the next page and the next page is reloaded. we have to relocate the element.
+            //reinitialize all the elements because the page gets refreshed next click
+            List<WebElement> tableRows = driver.findElements(By.xpath("//table[@id='resultTable']/tbody/tr"));
+
+            for (int i = 0; i < tableRows.size(); i++) {
+                String rowText = tableRows.get(i).getText();
+                if (rowText.contains(empId)) {
+                    flag = false;
+                    System.out.println(rowText);
+                    WebElement checkbox = driver.findElement(By.xpath("//table[@id='resultTable']/tbody/tr[" + i + "]/td[1]"));
+                    checkbox.click();
+                    WebElement deleteButton = driver.findElement(By.id("btnDelete"));
+                    deleteButton.click();
+                    WebElement confirmDelete = driver.findElement(By.id("dialogDeleteBtn"));
+                    confirmDelete.click();
+                    break;
+
+                }
+            }
+            if(flag){
+                WebElement nextButton = driver.findElement(By.linkText("Next"));
+                nextButton.click();
+            }
+
+        }
+
+
+    }
+}
